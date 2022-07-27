a95dfe3e2a5817144c2a69b9f9800c98af7c560b
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2022-07-27 21:24:13 +0700

    fix on hover page button

--- src/pages/GithubPage.tsx
+++ src/pages/GithubPage.tsx
@@ -5,9 +5,7 @@ import useGithubStateContext from "../hooks/useGithubStateContext";
 import githubService from "../services/githubService";
 import genUniqueId from "../utils/genUniqueId";
 
-type Props = {};
-
-const GithubPage = ({}: Props) => {
+const GithubPage = () => {
   const { repos, totalPage, currentPage } = useGithubStateContext();
   const githubDispatch = useGithubDispatchContext();
   const fetchGithubRepos = async () => {
@@ -21,48 +19,59 @@ const GithubPage = ({}: Props) => {
 
   return (
     <>
-      <section className="flex flex-col justify-center p-4">
-        <table className="mb-4 mt-4">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th className="min-w-320">Owner</th>
-            </tr>
-          </thead>
-          <tbody>
-            {repos?.length === 0 && (
+      <section className="flex h-full flex-col px-4 py-4">
+        <div className="pb-16">
+          <table className="w-full">
+            <thead>
               <tr>
-                <td colSpan={5}>Loading</td>
+                <th>Name</th>
+                <th className="hidden md:table-cell">Description</th>
+                <th className="min-w-320 hidden md:table-cell">Owner</th>
               </tr>
-            )}
-            {repos &&
-              repos.map((repo) => {
-                return (
-                  <tr key={`repo_${repo?.id}_${genUniqueId(5)}`}>
-                    <td>{repo.fullName}</td>
-                    <td>{repo.description}</td>
-                    <td>
-                      <div className="flex items-center space-x-2">
-                        <img
-                          className="p-1 w-10 h-10 rounded-full ring-1 ring-gray-300 dark:ring-gray-500"
-                          src={repo.owner?.avatarUrl}
-                        />
-                        <div>{repo.owner?.login}</div>
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-        <Pagination
-          totalPage={totalPage}
-          currentPage={currentPage}
-          onClick={(page) =>
-            githubDispatch({ type: "SET_PAGE", payload: page })
-          }
-        />
+            </thead>
+            <tbody>
+              {repos?.length === 0 && (
+                <tr>
+                  <td colSpan={5}>Loading</td>
+                </tr>
+              )}
+              {repos &&
+                repos.map((repo) => {
+                  return (
+                    <tr
+                      key={`repo_${repo?.id}_${genUniqueId(5)}`}
+                      className="min-h-56 h-14"
+                    >
+                      <td>{repo.fullName}</td>
+                      <td className="hidden md:table-cell">
+                        {repo.description}
+                      </td>
+                      <td className="hidden md:table-cell">
+                        <div className="flex items-center space-x-2">
+                          <img
+                            className="p-1 w-10 h-10 rounded-full ring-1 ring-gray-300 dark:ring-gray-500"
+                            src={repo.owner?.avatarUrl}
+                          />
+                          <div>{repo.owner?.login}</div>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+        <div className="fixed bg-white bottom-0 left-0 right-0">
+          <div className="flex w-full md:justify-center overflow-x-auto">
+            <Pagination
+              totalPage={totalPage}
+              currentPage={currentPage}
+              onClick={(page) =>
+                githubDispatch({ type: "SET_PAGE", payload: page })
+              }
+            />
+          </div>
+        </div>
       </section>
     </>
   );
